5140679819b5ed17434f15eafc419ebf8134980a
Mert <mert.izgahi@outlook.com>
2024-02-17 02:38:44 +0300

    footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,21 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <Flex
+            as="footer"
+            bg="primary.500"
+            color="white"
+            align="center"
+            justify="center"
+            p={8}
+        >
+            <Container maxW="container.xl" p={4}>
+                <Text align="center">© {new Date().getFullYear()} MeetX.</Text>
+                <Text align="center">All rights reserved.</Text>
+            </Container>
+        </Flex>
+    );
 }
 
 export default Footer;
